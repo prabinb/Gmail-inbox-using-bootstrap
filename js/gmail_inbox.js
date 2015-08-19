9778cba586fd1075e1d8857107d74861b44e2f3f
Prabin <prabin.banka@imaginea.com>
2015-08-19 17:50:51 +0530

    changed layout for mobile devices. Used Moustache for email templating

--- js/gmail_inbox.js
+++ js/gmail_inbox.js
@@ -31,5 +31,44 @@ $(document).ready(function(){
     $('#messageTabs > .active input[type=checkbox]').click();
   });
   
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    //var target = $(e.target).attr("href") // activated tab
+    $('#selectAllMessages').attr('checked', false);
+    $('#messageTabs > .active input[type=checkbox]').attr('checked', false);
+    
+  });
+  
+  $('[data-toggle=offcanvas]').click(function() {
+      $('.row-offcanvas').toggleClass('active');
+    });
+    
+  $('#emailTabs > li.active').click(function(){
+    var thisobj = $('#emailTabs');
+    if (thisobj.hasClass('mobile-open')){
+      thisobj.removeClass('mobile-open');
+    }else{
+      thisobj.addClass('mobile-open');
+    }
+  });
+  
 });
 
+
+function load() {
+  var template = $('#mailTemplate').html();
+  
+  var primary_inbox = Mustache.to_html(template, primary);
+  $('#primary').html(primary_inbox);
+  
+  var social_inbox = Mustache.to_html(template, social);
+  $('#social').html(social_inbox);
+  
+  var promotions_inbox = Mustache.to_html(template, promotions);
+  $('#promotions').html(promotions_inbox);
+  
+  var updates_inbox = Mustache.to_html(template, updates);
+  $('#updates').html(updates_inbox);
+  
+  var forums_inbox = Mustache.to_html(template, forums);
+  $('#forums').html(forums_inbox);
+};
